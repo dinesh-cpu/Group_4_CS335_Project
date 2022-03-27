79b7320a01dbadda8e98af3cc431f8b956ac69b9
shubhamkmt <kumawats@iitk.ac.in>
2022-03-27 12:46:14 +0530

    lexer changes

--- src/node.h
+++ src/node.h
@@ -1,19 +1,21 @@
+#include <string> 
+
 typedef struct node{
     long int id;
-    char* s;
+    std::string s;
     int child_count;
     struct node* child1;
     struct node* child2;
     struct node* child3;
 }node;
 
-FILE* outfile;
+extern FILE* outfile;
 extern FILE* yyin;
 
 void make_children(node* parent, node* child1, node* child2, node* child3);
-node* new_3_node(char* display, node* node1, node* node2, node* node3);
-node* new_2_node(char* display,node* node1,node* node2);
-node* new_2_Stringval_node(char* display, node* node1, node* node2);
-node* new_1_node(char* display,node* node1);
-node* new_leaf_node(char* val);
+node* new_3_node(std::string& display, node* node1, node* node2, node* node3);
+node* new_2_node(std::string& display,node* node1,node* node2);
+node* new_2_Stringval_node(std::string& display, node* node1, node* node2);
+node* new_1_node(std::string& display,node* node1);
+node* new_leaf_node(std::string& val);
 void printfile(struct node *root, FILE *file);
