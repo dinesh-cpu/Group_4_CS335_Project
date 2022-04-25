7a0e61b2f8ee52115f32b84fda6977cd1ad2266f
shubhamkmt <kumawats@iitk.ac.in>
2022-04-25 23:12:20 +0530

    added more testcase

---
+++ tests/test7.cpp
@@ -0,0 +1,17 @@
+int main()
+{
+    int x = 2;
+    int p=1;
+    if(1){
+        if(0){
+            p++;
+        }
+    }
+    else{
+        p++;
+    }
+    while(x==2){
+        p+=2;
+    }
+    return 0;
+}
