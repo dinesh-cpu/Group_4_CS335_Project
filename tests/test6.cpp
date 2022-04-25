af2aed9b6166eba69c7452af8eb66f852fe71c68
shubhamkmt <kumawats@iitk.ac.in>
2022-04-25 23:09:14 +0530

    added testcase

---
+++ tests/test6.cpp
@@ -0,0 +1,22 @@
+
+
+int main()
+{
+    int x = 2;
+    int p=1;
+    switch (x) {
+    case 1:
+        p++;
+        break;
+    case 2:
+        p+=2;
+        break;
+    case 3:
+        p+=3;
+        break;
+    default:
+        p+=4;
+        break;
+    }
+    return 0;
+}
